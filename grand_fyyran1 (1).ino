5fb95f802f001e675e35f8e6ee44d4124a6ba5a4
shreyaa-naik <shreyaa4950@gmail.com>
2025-08-11 10:15:17 +0530

    Add files via upload

---
+++ grand_fyyran1 (1).ino
@@ -0,0 +1,27 @@
+// C++ code
+//
+const int ledPins[]={2,3,4,5,6};
+const int numLeds=5;
+void setup()
+{
+  for(int i=0;i<numLeds;i++){
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop()
+{
+  for(int i=0;i<numLeds;i++){
+      digitalWrite(ledPins[i], HIGH);
+      delay(500); // Wait for 1000 millisecond(s)
+      digitalWrite(ledPins[i], LOW);
+    
+  }
+  for(int i=numLeds-1;i>=0;i--){
+      digitalWrite(ledPins[i], HIGH);
+      delay(500); // Wait for 1000 millisecond(s)
+      digitalWrite(ledPins[i], LOW);
+    }
+  
+  
+}
