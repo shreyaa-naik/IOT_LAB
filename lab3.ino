f2ff38da9258eee73d2b7c806e1fd6d81c8c3101
shreyaa-naik <shreyaa4950@gmail.com>
2025-09-01 09:48:40 +0530

    Add files via upload

---
+++ lab3.ino
@@ -0,0 +1,30 @@
+// C++ code
+//
+int Smoke_Sensor=0;
+void setup()
+{
+  pinMode(A0,INPUT);
+  Serial.begin(9600);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop()
+{
+  Smoke_Sensor=analogRead(A0);
+  Serial.println(Smoke_Sensor);
+  if(Smoke_Sensor>=100){
+    digitalWrite(7,HIGH);
+  } 
+  else{
+    digitalWrite(7,LOW);
+  }
+  if(Smoke_Sensor>=110){
+    digitalWrite(8,HIGH);
+    
+  }else{
+    digitalWrite(8,LOW);
+    
+  }
+  delay(10);
+}
